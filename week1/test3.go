b9104fd1ce4af34020aaaebad5cb3946e0a50e11
tanchira <57671539+tanchira@users.noreply.github.com>
2019-11-17 01:23:08 +0700

    add  modify test3

--- week1/test3.go
+++ week1/test3.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("____________________________________________________________________________________________")
-	fmt.Println("|                                                                                           |")
+	fmt.Println("______________________________________________________________")
+	fmt.Println("|          |           |             |            |          |")
 	fmt.Println("|  monday  |  Tuesday  |  Wednesday  |  Thursday  |  Friday  |                                                                            |")
 }
