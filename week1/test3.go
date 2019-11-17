a50b35a97b0ca847edb481c2092d6b09ee1b480f
tanchira <57671539+tanchira@users.noreply.github.com>
2019-11-17 15:14:38 +0700

    update test3

--- week1/test3.go
+++ week1/test3.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("___________________________________________________________________________")
-	fmt.Println("|        |            |             |             |            |          |")
-	fmt.Println("|  time  |   monday   |   Tuesday   |  Wednesday  |  Thursday  |  Friday  |")
+	fmt.Println("|         |            |             |             |            |          |")
+	fmt.Println("|  time   |   monday   |   Tuesday   |  Wednesday  |  Thursday  |  Friday  |")
 	fmt.Println("___________________________________________________________________________")
-	fmt.Println("|         |          |           |             |            |          |")
-	fmt.Println("| morning |  compro  |   math    |             |            |   sport  |")
+	fmt.Println("|         |            |             |             |            |          |")
+	fmt.Println("| morning |  compro    |    math     |             |            |   sport  |")
 	fmt.Println("_____________________________________________________________________")
 	fmt.Println("      |          |           |             |            |          |")
 	fmt.Println("      |  social  |  calculus |   english   |            |          |")
