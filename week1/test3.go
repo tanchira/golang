2741eea0aafc1dbb69a2f2f31a0adea1e22f0b9e
tanchira <57671539+tanchira@users.noreply.github.com>
2019-11-17 19:35:00 +0700

    update test3

--- week1/test3.go
+++ week1/test3.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("___________________________________________________________________________")
-	fmt.Println("|         |            |             |             |            |          |")
-	fmt.Println("|  time   |   monday   |   Tuesday   |  Wednesday  |  Thursday  |  Friday  |")
-	fmt.Println("___________________________________________________________________________")
+	fmt.Println("________________________________________________________________________________")
+	fmt.Println("|             |            |             |             |            |          |")
+	fmt.Println("|     time    |   monday   |   Tuesday   |  Wednesday  |  Thursday  |  Friday  |")
+	fmt.Println("________________________________________________________________________________")
 	fmt.Println("|             |            |             |             |            |          |")
 	fmt.Println("|   morning   |  compro    |    math     |             |            |   sport  |")
 	fmt.Println("________________________________________________________________________________")
