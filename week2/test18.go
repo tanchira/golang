4e7fa768a766905fe43433bf68d403b8f8a3df45
tanchira <57671539+tanchira@users.noreply.github.com>
2019-11-23 11:44:55 +0700

    Update test18.go

--- week2/test18.go
+++ week2/test18.go
@@ -7,5 +7,5 @@ import (
 func main() {
 	fmt.Printf("10 is of type %T\n", 10)
 	fmt.Printf("10 is of type %T\n", string(10))
-	fmt.Printf("10 is of type %T\n")
+	fmt.Printf("10 is of type %T\n", float64(10))
 }
