24abb1b1a7be132cc0391b7ef2bc520e1350a0ce
tanchira <57671539+tanchira@users.noreply.github.com>
2019-11-23 10:53:31 +0700

    Update test6.go

--- week2/test6.go
+++ week2/test6.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 )
 
 func main() {
+	startA := time.Now()
+	var a string
+	for i := 0; i < 10000; i++ {
+		a += "a"
+	}
+	fmt.Println("a", time.Since(startA))
+
+	startB := time.Now()
 	var b strings.Builder
-	b.WriteString("Hello")
-	b.WriteString(" ")
-	b.WriteString("world")
-	fmt.Println(b.String())
+	for i := 0; i < 10000; i++ {
+		b.WriteString("b")
+	}
+	fmt.Println("b", time.Since(startB))
 
 }
