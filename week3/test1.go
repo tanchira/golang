00afe63cbdf895726385beeb056c93174445cf6e
tanchira <57671539+tanchira@users.noreply.github.com>
2019-11-29 18:10:40 +0700

    Update test1.go

--- week3/test1.go
+++ week3/test1.go
@@ -6,6 +6,7 @@ func main() {
 	fmt.Printf("%v \n", 17)
 	fmt.Printf("%d \n", 18)
 	fmt.Printf("%g \n", 19.259)
+	fmt.Print("%s \n", "hello")
 	fmt.Println("%s", "hello")
 	fmt.Printf("%s", "hello")
 
