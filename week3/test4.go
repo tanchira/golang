4af77cb33ece138f0f651c793676304fd4fc4f72
tanchira <57671539+tanchira@users.noreply.github.com>
2019-11-29 21:34:49 +0700

    Update test4.go

--- week3/test4.go
+++ week3/test4.go
@@ -13,6 +13,6 @@ func main() {
 	fmt.Printf("%05d \n", 14)
 	fmt.Printf("%010d \n", 22)
 	fmt.Printf("%05g \n", 22.21)
-	fmt.Printf("%04t", 15 > 14)
-	fmt.Printf("%08t", 16 < 15)
+	fmt.Printf("%04t \n", 15 > 14)
+	fmt.Printf("%08t \n", 16 < 15)
 }
