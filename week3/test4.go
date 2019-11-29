dbeb8393f1de25e9dda1f5748852353c12f13e36
tanchira <57671539+tanchira@users.noreply.github.com>
2019-11-29 21:17:56 +0700

    Update test4.go

--- week3/test4.go
+++ week3/test4.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	fmt.Printf("%+d \n", 10)
 	fmt.Printf("%-d \n", 100)
-	fmt.Printf("%-d", 10)
-	fmt.Printf("%9d", 10)
+	fmt.Printf("%-d \n", 10)
+	fmt.Printf("%9d \n", 10)
+	fmt.Printf("%15d \n", 10)
 }
