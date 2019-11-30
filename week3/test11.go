b14ab63a92d5eb77a0990ccada95b84e4df05ff7
tanchira <57671539+tanchira@users.noreply.github.com>
2019-11-30 21:41:53 +0700

    Update test11.go

--- week3/test11.go
+++ week3/test11.go
@@ -5,4 +5,5 @@ import "fmt"
 func main() {
 	n, e := fmt.Println("tanchira", "Paewkrathok", 123, 456, 789)
 	fmt.Println("number of bypes written :", n)
+	fmt.Println("write error encountered :", e)
 }
