57b3a7c0dfab177caa975d6c9d0026459b9eef6e
tanchira <57671539+tanchira@users.noreply.github.com>
2019-11-30 20:45:10 +0700

    Update test6.go

--- week3/test6.go
+++ week3/test6.go
@@ -1,14 +1,17 @@
 package main
 
-func main() {
-	fmt.Printf("My name is %s, I am %d years old \n" ,"bam",19)
-	fmt.Printf("type = %T \n",11)
-	fmt.Printf("type = %T \n",3.10159265359)
-	fmt.Printf("Pi =%2.2f \n",3.10159265359)
-	fmt.Printf("Pi =%9.f \n",3.10159265359)
-	fmt.Printf("Pi =%-9.f \n",3.10159265359)
-	fmt.Printf("Pi =%09.f \n",3.10159265359)
-	fmt.Printf("Pi =%09.2f \n",3.10159265359)
-	fmt.Printf("Pi =%E \n",3.10159265359)
+import "fmt"
 
-	fmt.Printf("1>2 =%t \n",1>2)
+func main() {
+	fmt.Printf("My name is %s, I am %d years old \n", "bam", 19)
+	fmt.Printf("type = %T \n", 11)
+	fmt.Printf("type = %T \n", 3.10159265359)
+	fmt.Printf("Pi =%2.2f \n", 3.10159265359)
+	fmt.Printf("Pi =%9.f \n", 3.10159265359)
+	fmt.Printf("Pi =%-9.f \n", 3.10159265359)
+	fmt.Printf("Pi =%09.f \n", 3.10159265359)
+	fmt.Printf("Pi =%09.2f \n", 3.10159265359)
+	fmt.Printf("Pi =%E \n", 3.10159265359)
+
+	fmt.Printf("1 > 2 =%t \n", 1 > 2)
+}
