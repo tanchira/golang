6ba47b48ebc5deeb5cf378ef03bbef88e18d8d67
tanchira <57671539+tanchira@users.noreply.github.com>
2019-11-30 21:05:14 +0700

    Update test7.go

--- week3/test7.go
+++ week3/test7.go
@@ -8,5 +8,6 @@ func main() {
 	fmt.Printf("Pi =%03.2f \n", 1.23456789)
 	fmt.Printf("Pi = %011.4f \n", 1.23456789)
 	fmt.Printf("Pi =%02.f \n", 1.23456789)
+	fmt.Printf("Pi =%05.f \n", 1234.56789)
 
 }
