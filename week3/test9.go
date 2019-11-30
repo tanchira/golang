5fc5ef4079b1392f1e98b1ad7f49920e50bb4e7f
tanchira <57671539+tanchira@users.noreply.github.com>
2019-11-30 21:20:59 +0700

    Update test9.go

--- week3/test9.go
+++ week3/test9.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	n, e := fmt.Print("tanchira", "Paewkrathok", 123, 456, "\n")
+	n, e := fmt.Print("tanchira", "Paewkrathok", 123, 456,789,123,456,789 "\n")
 	fmt.Print(" munber of bytes written :", n, "\n")
 	fmt.Print("write error encountered :", e, "\n")
 }
