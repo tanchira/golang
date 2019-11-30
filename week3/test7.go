0bf1b5e84bef99c8811006346cb2ac22bd2c80cd
tanchira <57671539+tanchira@users.noreply.github.com>
2019-11-30 21:01:34 +0700

    Update test7.go

--- week3/test7.go
+++ week3/test7.go
@@ -5,5 +5,6 @@ import "fmt"
 func main() {
 	fmt.Printf("My name is %s , I am %d old ,age I  > brother = %t  \n ", "bam", 19, 19 > 8)
 	fmt.Printf("Pi = %08.5f \n", 12.3456789)
+	fmt.Printf("Pi =%03.2 \n", 1.23456789)
 
 }
