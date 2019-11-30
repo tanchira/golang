cf81fd268fd700792110d45b02b2912164e95cfe
tanchira <57671539+tanchira@users.noreply.github.com>
2019-11-30 21:34:43 +0700

    Update test8.go

--- week3/test8.go
+++ week3/test8.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	n, e := fmt.Print("Hello", "World", 123, 456 "\n")
-	fmt.Print("number of bytes written :", n,"\n")
-	fmt.Print("write error encountered :", e,"\n")
+	n, e := fmt.Print("Hello", "World", 123, 456, "\n")
+	fmt.Print("number of bytes written :", n, "\n")
+	fmt.Print("write error encountered :", e, "\n")
 }
