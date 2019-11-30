76ce4bca004c068323643abe4289b84b0c96dc51
tanchira <57671539+tanchira@users.noreply.github.com>
2019-11-30 20:41:01 +0700

    Update test6.go

--- week3/test6.go
+++ week3/test6.go
@@ -4,4 +4,4 @@ func main() {
 	fmt.Printf("My name is %s, I am %d years old \n" ,"bam",19)
 	fmt.Printf("type = %T \n",11)
 	fmt.Printf("type = %T \n",3.10159265359)
-	
+	fmt.Printf("Pi =%2.2f \n",3.10159265359)
