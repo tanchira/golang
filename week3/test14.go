ac873608d40997727a18ff8f712195561537f8d4
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-01 13:55:41 +0700

    Update test14.go

--- week3/test14.go
+++ week3/test14.go
@@ -4,10 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Print("imput name :  \n")
+	var name string
 	fmt.Print("input age :  \n")
 	fmt.Print("input height :  \n")
 	fmt.Print("input weight :  \n")
-	var name string
+
 	var age int
 	var height float32
 	var weight float32
