88f6b55af48ecb7eb79dcea93cd2e58fb240ed3e
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-01 13:44:43 +0700

    Update test13.go

--- week3/test13.go
+++ week3/test13.go
@@ -8,8 +8,8 @@ func main() {
 	var age int
 	var height float32
 	var weight float32
-	n, err := fmt.Scan(&name, &age, &heigth, &weight)
-	fmt.Println(name, age, height, weight)
+	n, err := fmt.Scan(&name, &age, &weight, &heigth)
+	fmt.Println(name, age, weight, height)
 	fmt.Println(`number of argument`, n)
 	fmt.Println(`error`, err)
 }
