52f468f2e95c1495a9b7840003db0d3260822489
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-01 13:43:30 +0700

    Update test13.go

--- week3/test13.go
+++ week3/test13.go
@@ -10,4 +10,6 @@ func main() {
 	var weight float32
 	n, err := fmt.Scan(&name, &age, &heigth, &weight)
 	fmt.Println(name, age, height, weight)
+	fmt.Println(`number of argument`, n)
+	fmt.Println(`error`, err)
 }
