c7882570d45872bcb8c78b065e4efb973c2add52
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-05 10:07:10 +0700

    Update test1.go

--- week4/test1.go
+++ week4/test1.go
@@ -1,13 +1,6 @@
 package test1
 
-import "fmt"
-
-func main() {
-	Number := Average(3, 5)
-	fmt.Print(Number)
-}
-
-func Average(num ...int) int {
-	var sum int
+func Average(num int) int {
+	return 4
 
 }
