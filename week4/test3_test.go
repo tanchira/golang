2b7cde9a217ba34824a2a317b60185b732e6b55c
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-05 10:40:15 +0700

    Update test3_test.go

--- week4/test3_test.go
+++ week4/test3_test.go
@@ -3,8 +3,8 @@ package test1
 import "testing"
 
 func TestHel(t *testing.T) {
-	got := "Hello"
-	want := "Hello,World"
+	got := Hello()
+	want := "Hello World"
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
