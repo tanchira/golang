a16ad9cf681c517731408a94fe17249b8fcd8e65
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-05 09:07:41 +0700

    Update test1.go

--- week4/test1.go
+++ week4/test1.go
@@ -1,15 +1,13 @@
 package test1
 
-func main()  {
-	Number := Average(3,5)
+import "fmt"
+
+func main() {
+	Number := Average(3, 5)
 	fmt.Print(Number)
 }
 
-
-
-func Average(num ...int) int {
-	v num int
-	num := 3,5
-	return num == v
+func Average(Number ...int) int {
+	return Number
 
 }
