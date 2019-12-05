e374a6d297f520f9999d27bf0292ded36c093d13
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-05 10:07:16 +0700

    Update test1_test.go

--- week4/test1_test.go
+++ week4/test1_test.go
@@ -3,9 +3,14 @@ package test1
 import "testing"
 
 func TestAverage(t *testing.T) {
-	v := Average(3, 5)
+	v := Average(3)
 	if v != 4 {
 		t.Error("Average of 3,5 is 4, not", v)
-	}
 
+		v := Average(5)
+		if v != 4 {
+			t.Error("Average of 3,5 is 4, not", v)
+		}
+
+	}
 }
