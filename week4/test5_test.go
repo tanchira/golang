12a43ce2669fd7915a6e92d30b1417ea26b2aba3
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-06 21:24:02 +0700

    Update test5_test.go

--- week4/test5_test.go
+++ week4/test5_test.go
@@ -4,9 +4,12 @@ import "testing"
 
 func TestRomanNumerals(t *testing.T) {
 	cases := []struct {
-		Description  string
-		Arabic       int
-		Want         string
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
 	}
 
 }
