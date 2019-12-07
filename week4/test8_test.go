1d4c0b2d5001c88f0ac7b8369ad1ecc26da660ac
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-07 19:34:30 +0700

    Update test8_test.go

--- week4/test8_test.go
+++ week4/test8_test.go
@@ -3,5 +3,13 @@ package test1
 import "testing"
 
 func TestSum(t *testing.T) {
+	t.Run("Hello World", func(t *tesing.T) {
+		want := true
+		got := Sum("Hello World")
+		if want != got {
+			t.Error("want true but got fales", want, got)
+		}
+
+	})
 
 }
