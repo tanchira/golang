beb2d991b1976ec7531c8ae438d84f37cf708b03
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-07 20:57:39 +0700

    Update test8.go

--- week4/test8.go
+++ week4/test8.go
@@ -1,6 +1,6 @@
 package test1
 
-func Sum(x string, y string) bool {
-	return x != "Hell World  " == false
-	return y != "tanchira" == false
+func Sum(x string) bool {
+	return x == "Hell World ,tanchira" == false
+
 }
