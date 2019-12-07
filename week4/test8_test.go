d8fcb87b6a18f0020a1e98593cd05749726a4cb0
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-07 19:37:43 +0700

    Update test8_test.go

--- week4/test8_test.go
+++ week4/test8_test.go
@@ -14,6 +14,9 @@ func TestSum(t *testing.T) {
 	t.Run("Happy birthday", func(t *testing.T) {
 		want := false
 		got := Sum("Happy birthday")
+		if want != got {
+			t.Error("want false but got true", want, got)
+		}
 
 	})
 
