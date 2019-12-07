9c67389a0026f44895b54eb7f92a8c66142ee887
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-07 18:18:57 +0700

    Update test6_test.go

--- week4/test6_test.go
+++ week4/test6_test.go
@@ -3,5 +3,12 @@ package test1
 import "testing"
 
 func TestFind(t *testing.T) {
+	t.Run("Hello World", func(t *testing.T) {
+		want := true
+		got := Find("Hello World")
+		if got != want {
+			t.Errorf("\n got %v \n want %v")
+		}
+	})
 
 }
