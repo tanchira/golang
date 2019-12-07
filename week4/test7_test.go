f2a06eae400cc587ff587d8345ccf06b9cce3d4f
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-07 19:24:21 +0700

    Update test7_test.go

--- week4/test7_test.go
+++ week4/test7_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestText(t *testing.T) {
 	Run."Happ birthday",func (t *testing.T)  {
 		want := false
-		got :=Find("Heppy birthday")
+		got :=Text("Heppy birthday")
 		if want != got{
 			t.Errorf("want fales but got ture",want.got)
 		}
