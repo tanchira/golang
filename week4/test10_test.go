0e33b56dc1f0bbf6950ea0c0fae6591c3115fb8e
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-08 09:58:56 +0700

    Update test10_test.go

--- week4/test10_test.go
+++ week4/test10_test.go
@@ -2,11 +2,11 @@ package test1
 
 import "testing"
 
-func TestNumber(t *testing.T) {
+func TestDelete(t *testing.T) {
 	want := 5
-	got := (8,13)
+	got := Delete(8, 13)
 	if want != got {
-		t.Error("got 8 and 13 not 5",want,got)
+		t.Error("got 8 and 13 not 5", want, got)
 	}
 
 }
