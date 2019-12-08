69b45d5bc03f19dad97aeae46515b81355d7c5a5
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-08 09:46:44 +0700

    Update test9_test.go

--- week4/test9_test.go
+++ week4/test9_test.go
@@ -8,8 +8,8 @@ func TestNumber(t *testing.T) {
 	if want != got {
 		t.Error("error got not to want")
 	}
-	want := 2
-	got := Number(2, 4)
+	want = 2
+	got = Number(2, 4)
 	if want != got {
 		t.Error("error got not to want")
 	}
