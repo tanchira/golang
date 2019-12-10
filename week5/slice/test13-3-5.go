232e8fe928d13a8ad0ac9f7991171675412ab0de
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-10 18:20:52 +0700

    Update test13-3-5.go

--- week5/slice/test13-3-5.go
+++ week5/slice/test13-3-5.go
@@ -1,7 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
 	human := []string{"bulma", "chi-chi", "videl"}
 	name := []string{"goku", "gohan"}
 	name = append(human, name...)
+	fmt.Println(name)
 }
