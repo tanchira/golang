c64b4d34609966df8e2ff7125e070cbdedb2884c
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-10 16:32:32 +0700

    Create test3-5.go

---
+++ week5/array/test3-5.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(numbers)
+	fmt.Println(primes)
+}
