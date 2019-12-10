6b18a8663a65abd329f4729b70056808eb096075
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-10 14:45:35 +0700

    Update test3-5.go

--- week5/test3-5.go
+++ week5/test3-5.go
@@ -1,5 +1,9 @@
 package main
 
+import "fmt"
+
 func main() {
-	number := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	numbers := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(nmubers)
 }
