baf6ce558cdb2135775fb65871857e854b5f5fcb
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-10 16:32:58 +0700

    Delete test1-5.go

--- week5/test1-5.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	student := []string{"Goku", "Gohan", "Vegeta", "Piccolo", "Krillin", "Yamcha", "Bluma", "Videl", "Oolong", "Puar"}
-
-	fmt.Println(student)
-}
