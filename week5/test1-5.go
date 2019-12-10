c05626c1735495b4de9e1111023d9f7cb7e08ed7
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-10 14:31:40 +0700

    Update test1-5.go

--- week5/test1-5.go
+++ week5/test1-5.go
@@ -1,1 +1,9 @@
 package main
+
+import "fmt"
+
+func main() {
+	student := []string{"Goku", "Gohan", "Vegeta", "Piccolo", "Krillin", "Yamcha", "Bluma", "Videl", "Oolong", "Puar"}
+
+	fmt.Println(student)
+}
