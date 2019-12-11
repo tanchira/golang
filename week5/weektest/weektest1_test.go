b5d31d570b5cda6b943f2246ba7bafefe7c85258
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-11 20:48:14 +0700

    Create weektest1_test.go

---
+++ week5/weektest/weektest1_test.go
@@ -0,0 +1,23 @@
+package week5_6
+
+import (
+	"testing"
+)
+
+func TestFizzWord(t *testing.T) {
+	testCases := []struct {
+		input [2]int
+		want  string
+	}{
+		{[2]int{1, 1}, "Fizz"},
+		{[2]int{2, 1}, "Fizz"},
+		{[2]int{2, 3}, "2"},
+		{[2]int{9, 7}, "9"},
+	}
+	for _, test := range testCases {
+		got := FizzWord(test.input[0], test.input[1])
+		if got != test.want {
+			t.Errorf("\nunexpected\n\tgot: %v\n\twant: %v", got, test.want)
+		}
+	}
+}
