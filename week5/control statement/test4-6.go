a28d42969efef76fa5468ed38320dcd8450d9ef6
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-11 20:20:08 +0700

    Update test4-6.go

--- week5/control statement/test4-6.go
+++ week5/control statement/test4-6.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	for essay := 1; essay <= 21; essay = essay + 1 {
+	for essay := 1; essay <= 20; essay = essay + 1 {
 		if essay%2 == 0 {
 			fmt.Println(essay, "even")
 		} else {
