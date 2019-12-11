798bb47db294d5a8b8d28d0d850c9f9e006c2987
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-11 20:53:50 +0700

    Update weektest1.go

--- week5/weektest/weektest1.go
+++ week5/weektest/weektest1.go
@@ -5,11 +5,9 @@ import "strconv"
 //FizzWord รับพารามิเตอร์ 2 ตัว เมื่อนำตัวแรกมาหารด้วยตัวที่สองลงตัวจะคืนค่าเป็น "Fizz" หากไม่ลงตัวจะคืนค่าเป็นพารามิตเอร์ตัวแรกแต่มีไทป์เป็น string
 
 func FizzWord(number int, mod int) string {
-	for number := 1; number < 10; number = number + 1 {
-		if number%mod == 0 {
-			return "fizz"
-		}
 
+	if number%mod == 0 {
+		return "fizz"
 	}
 	return strconv.Itoa(number)
 }
