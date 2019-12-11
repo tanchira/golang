2194846a5c023abfc4294802db55ca9aadb95977
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-11 20:11:13 +0700

    Update test3-6.go

--- week5/control statement/test3-6.go
+++ week5/control statement/test3-6.go
@@ -9,9 +9,18 @@ func main() {
 	}
 }
 
+
+import "fmt"
 func main() {
-	essay := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	for i, v := range essay {
-		fmt.Printf("%d \n", i, v)
+	essay := []bool{true, false, true, true, false, true}
+	for _, essay := range essay {
+		fmt.Printf("%t \n", essay)
 	}
 }
+
+//func main() {
+//	essay := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+//	for i, v := range essay {
+//		fmt.Printf("%d \n", i, v)
+//	}
+//}
