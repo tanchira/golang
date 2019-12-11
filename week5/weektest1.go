088be2bf015e269411d262704ebbec2e9c91b7b1
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-11 20:46:28 +0700

    Update weektest1.go

--- week5/weektest1.go
+++ week5/weektest1.go
@@ -5,6 +5,7 @@ import "strconv"
 //FizzWord รับพารามิเตอร์ 2 ตัว เมื่อนำตัวแรกมาหารด้วยตัวที่สองลงตัวจะคืนค่าเป็น "Fizz" หากไม่ลงตัวจะคืนค่าเป็นพารามิตเอร์ตัวแรกแต่มีไทป์เป็น string
 
 func FizzWord(number int, mod int) string {
+
 	if number%mod == 0 {
 		return "fizz"
 	}
