1b85c528371feabc3c84d2ab39fdfbb1ca837ff0
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-11 20:12:53 +0700

    Update test3-6.go

--- week5/control statement/test3-6.go
+++ week5/control statement/test3-6.go
@@ -11,7 +11,7 @@ import "fmt"
 
 func main() {
 	essay := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	for _, v := range essay {
-		fmt.Printf(" \n", v)
+	for _, essay := range essay {
+		fmt.Printf(" \n", essay)
 	}
 }
