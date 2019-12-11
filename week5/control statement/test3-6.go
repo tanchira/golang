f37902be936bdc9d85dff1b9eab39fb597fdd1da
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-11 20:15:45 +0700

    Update test3-6.go

--- week5/control statement/test3-6.go
+++ week5/control statement/test3-6.go
@@ -9,12 +9,12 @@ import "fmt"
 //}
 //}
 
-func main() {
-	essay := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	for _, essay := range essay {
-		fmt.Printf(" \n", essay)
-	}
-}
+//func main() {
+//	essay := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+//	for _, essay := range essay {
+//		fmt.Printf(" \n", essay)
+//	}
+//}
 
 func main() {
 	essay := []string{"a", "b", "c", "d", "e"}
