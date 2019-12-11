b14e37a55b6728df1ef324a2655b826334bec3a9
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-11 20:29:43 +0700

    Update testaction3.go

--- week5/testaction3.go
+++ week5/testaction3.go
@@ -1,11 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
-	for text := 1; text<1000; text=text+1{
-		if text%3 ==0 {
-			fmt.Println(text."fizz")
-		}else {
-			fmt.Println(text,"buzz")
+	for text := 1; text < 1000; text = text + 1 {
+		if text%3 == 0 {
+			fmt.Println(text, "fizz")
+		} else {
+			fmt.Println(text, "buzz")
 		}
 	}
 }
