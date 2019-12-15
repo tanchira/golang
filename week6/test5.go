011a45e92e85d8408585956292ac3238bf141983
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-15 18:47:34 +0700

    Update test5.go

--- week6/test5.go
+++ week6/test5.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	number := []int(1, 2, 3, 4, 5, 6, 7, 8, 9)
+	number := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	total := sum(number...)
 	fmt.Println(total)
 }
