cce4c50dc805a700fbca9d2a651f9a97bee36ff0
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-15 18:32:36 +0700

    Update test2.2.go

--- week6/test2.2.go
+++ week6/test2.2.go
@@ -1,10 +1,13 @@
 package main
 
+import "fmt"
+
 func subtract(text1 string, text2 string, text3 string) string {
 	include := "text3,text2, text1"
 	return include
 }
 
 func main() {
-	subtract("M,A,B")
+	include := subtract("M,A,B")
+	fmt.Println(subtract)
 }
