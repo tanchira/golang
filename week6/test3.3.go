480dca1f9f4e157957d0c37e5b7810ff128211d1
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-15 18:19:29 +0700

    Update test3.3.go

--- week6/test3.3.go
+++ week6/test3.3.go
@@ -1,9 +1,12 @@
 package main
 
+import "fmt"
+
 func swap(sum1, sum2, sum3 int) (int, int, int) {
 	return sum3, sum2, sum1
 }
 
 func main() {
 	sum1, sum2, sum3 := swap(30, 20, 10)
+	fmt.Println(sum1, sum2, sum3)
 }
