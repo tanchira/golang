9240eaa0c6ff80d9b5f6e431dc04ac33a3ff389f
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-22 22:25:13 +0700

    Update max_test.go

--- week7/max/max_test.go
+++ week7/max/max_test.go
@@ -1,8 +1,11 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestMax(t *testing.T) {
 	max := Max(89, 66, 11, 44, 77, 66, 24, 91, 99, 87, 39, 95, 45)
-
+	fmt.Println(max)
 }
