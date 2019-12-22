b792fe5113087a5c1bc9d4e16b128d382501d293
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-22 21:51:39 +0700

    Update max.go

--- week7/max.go
+++ week7/max.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func Max(number ...int) int {
 	max := number[0]
 	for text := 0; text < len(number); text++ {
@@ -13,4 +15,5 @@ func Max(number ...int) int {
 func main() {
 	panic("error")
 	max := Max(4, 8, 9, 3, 21, 5, 10, 56, 84, 71, 32)
+	fmt.Println(max)
 }
