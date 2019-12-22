52c67ae73d495d858c18c7b182601ec51848ff7d
tanchira <57671539+tanchira@users.noreply.github.com>
2019-12-22 21:46:26 +0700

    Update max.go

--- week7/max.go
+++ week7/max.go
@@ -2,5 +2,7 @@ package main
 
 func Max(number ...int) int {
 	max := number[0]
-	for text := 0; text < len(number);text++
+	for text := 0; text < len(number);text++{
+		if number[text] > max
+	}
 }
