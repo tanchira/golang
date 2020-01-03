7c6af4257537f9653d115eda847a73a7f1396b15
tanchira <57671539+tanchira@users.noreply.github.com>
2020-01-03 11:55:48 +0700

    Update salary.go

--- action/salary.go
+++ action/salary.go
@@ -6,7 +6,7 @@ func main() {
 	salary := 4000
 	deduct := 10000 / 100 * 10
 	deduct2 := 5001 / 100 * 5
-	if salary > 10000 {
+	if deduct >= 10000 {
 		fmt.Println(deduct)
 	} else if deduct2 >= 10000 {
 		fmt.Println(deduct2)
