a806a8e0f3eadab3c2913aae658f8eeae57a0d61
tanchira <57671539+tanchira@users.noreply.github.com>
2020-01-04 21:43:37 +0700

    Update bonus.go

--- action/bonus.go
+++ action/bonus.go
@@ -9,11 +9,13 @@ func main() {
 	fmt.Println("***********************************")
 	var no int
 	fmt.Println("  No (ลำดับพนักงาน)	:	")
+	fmt.Scan(&no)
 	var name string
 	fmt.Println("  Name (ชื่อพนักงาน)	 :	")
+	fmt.Scan(&name)
 	var summit int
 	fmt.Println("  Summit (ยอดขาย)	:	", summit)
-	fmt.Scan(&no, &name, &summit)
+	fmt.Scan(&summit)
 	fmt.Println("***********************************")
 	if summit >= 50000 {
 		fmt.Println(" Bonus (โบนัส)         =      ", Hugesales)
