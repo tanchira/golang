f3cb1ca4c23fa358e0b2b19fd0ecf5aa96892371
tanchira <57671539+tanchira@users.noreply.github.com>
2020-01-04 21:22:37 +0700

    Update salary.go

--- action/salary.go
+++ action/salary.go
@@ -10,7 +10,7 @@ func main() {
 	deduct2 := 5001 / 100 * 5
 	if salary >= 10000 {
 		fmt.Println("หักภาษีทั้งหมด", deduct)
-	} else if salary >= 5001 {
-		fmt.Println(deduct2, "ให้หักภาษีทั้งหมด", deduct2)
+	} else if salary <= 5001 {
+		fmt.Println("ให้หักภาษีทั้งหมด", deduct2)
 	}
 }
