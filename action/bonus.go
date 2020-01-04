d4026884e25575fb34855d0804ab598b295240a6
tanchira <57671539+tanchira@users.noreply.github.com>
2020-01-04 20:25:46 +0700

    Update bonus.go

--- action/bonus.go
+++ action/bonus.go
@@ -6,6 +6,9 @@ func main() {
 	want := 50000
 	Hugesales := 50000 / 100 * 30
 	sales := 49999 / 100 * 10
+	fmt.Println("			Report  Bonus			")
+	
+	
 	if want >= 50000 {
 		fmt.Println(Hugesales)
 	} else {
