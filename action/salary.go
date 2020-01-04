4a95947f604fe8de709b57a66c6af218fb51f215
tanchira <57671539+tanchira@users.noreply.github.com>
2020-01-04 21:19:54 +0700

    Update salary.go

--- action/salary.go
+++ action/salary.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	var salary int
-	fmt.Scan(&salary)
 	fmt.Println("salary : ")
+	fmt.Scan(&salary)
 	deduct := 10000 / 100 * 10
 	deduct2 := 5001 / 100 * 5
 	if salary >= 10000 {
