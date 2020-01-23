20bc2bfc5e09bfb6039c8417ad162e0175cf02a2
tanchira <57671539+tanchira@users.noreply.github.com>
2020-01-23 14:58:39 +0700

    Update test3.go

--- week10/test3.go
+++ week10/test3.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func test(txt int, txt2 string) {
-	for First := 1; First < 2; First++ {
-		fmt.Println(txt2)
+	for First := 1; First < 4; First++ {
+		fmt.Println(First, txt2)
 	}
 
 }
 func main() {
 	go test(12, "bam")
 	var inputone int
-	var inputtoo string
+	var inputtwo string
 
-	fmt.Scanln(&inputone, &inputtoo)
+	fmt.Scanln(&inputone, &inputtwo)
 }
