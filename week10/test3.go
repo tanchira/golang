4df257cced6c278e6a1df5afdd2e3c4151621815
tanchira <57671539+tanchira@users.noreply.github.com>
2020-01-23 14:47:12 +0700

    Update test3.go

--- week10/test3.go
+++ week10/test3.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func test(txt int, txt2 string) {
 	for First := 1; First < 2; First++ {
-		fmt.Println(First, ".", txt2, txt)
+		fmt.Println(txt2)
 	}
 
 }
 func main() {
-	go test("10")
+	go test(12, "bam")
 
 }
