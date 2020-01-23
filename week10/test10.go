c0aa6738a8f34e27f2d3486bad1ba4b64e9c91f9
tanchira <57671539+tanchira@users.noreply.github.com>
2020-01-23 17:23:18 +0700

    Update test10.go

--- week10/test10.go
+++ week10/test10.go
@@ -12,13 +12,14 @@ func sum(c chan int, number ...int) {
 }
 func printer(c chan int) {
 	number := <-c
+	fmt.Println(number)
 }
 func main() {
 	c := make(chan int)
 	go printer(c)
 	go printer(c)
-	go sum(c, 1, 2, 3)
-	go sum(c, 10, 11)
+	go sum(c, 1, 2, 3, 4, 5)
+	go sum(c, 20, 20)
 	var input string
 	fmt.Scan(&input)
 }
