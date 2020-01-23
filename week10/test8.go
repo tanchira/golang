b984c9f8ee80112026aeb4401f5609b0833e83f0
tanchira <57671539+tanchira@users.noreply.github.com>
2020-01-23 16:26:34 +0700

    Update test8.go

--- week10/test8.go
+++ week10/test8.go
@@ -18,7 +18,7 @@ func read(data *int, mutex *sync.Mutex, wg *sync.WaitGroup) {
 	start := time.Now()
 	defer wg.Done()
 	defer mutex.Unlock()
-
+	mutex.Lock()
 }
 func main() {
 	var mutex sync.Mutex
