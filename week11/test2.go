119352fd8953d396d29618ce2180fe602740b5dd
tanchira <57671539+tanchira@users.noreply.github.com>
2020-01-31 11:40:05 +0700

    Update test2.go

--- week11/test2.go
+++ week11/test2.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"strings"
 )
 
 func main() {
 	reader := strings.NewReader("Hello Tanchira")
-	text := make([]byte, 2)
+	text := make([]byte, 3)
 	for {
 		n, err := reader.Read(text)
 		if err == io.EOF {
 			break
 		}
+		fmt.Println(string(text[:n]))
 	}
 }
