ce738eb59d99724a3e52e5212da51b7fd1940471
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-01 18:24:21 +0700

    Create test9.go

---
+++ week11/test9.go
@@ -0,0 +1,15 @@
+package main
+
+import "os"
+
+func getDrives() (r []string) {
+	for _, drive := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
+		f, err := os.Open(string(drive) + ":\\")
+		if err == nil {
+			d := string(drive) + ":/"
+			r = append(r, d)
+			f.Close()
+		}
+	}
+	return
+}
