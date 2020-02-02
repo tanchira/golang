55eac2773bfcf0fca064d9775f2840fefb8ff11c
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-02 14:36:12 +0700

    Update confused.go

--- week12/card/confused.go
+++ week12/card/confused.go
@@ -1,5 +1,6 @@
 package card
 
-func card() int{
-	num := []string{"A","2","3","4","5","6","7","8","9","10","J","Q","K"}
+func card() int {
+	num := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+	ren := make([]string, 6)
 }
