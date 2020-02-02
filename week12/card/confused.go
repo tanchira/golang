148e9bd099dce49309e56cd3cfa0744376e4e955
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-02 14:39:03 +0700

    Update confused.go

--- week12/card/confused.go
+++ week12/card/confused.go
@@ -1,6 +1,12 @@
 package card
 
+import "math/rand"
+
 func card() int {
 	num := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
 	ren := make([]string, 6)
+	for i := 0; i < 5; i++ {
+		renn := num[rand.Intn(len(num))]
+		ren = append(ren, renn)
+	}
 }
