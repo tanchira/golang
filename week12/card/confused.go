594646bd9a75932a48ad85aecf2aed782f935249
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-02 15:07:59 +0700

    Update confused.go

--- week12/card/confused.go
+++ week12/card/confused.go
@@ -10,8 +10,8 @@ func main() int {
 	num := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
 	ren := make([]string, 6)
 	for i := 0; i < 5; i++ {
-		renn := num[rand.Intn(len(num))]
-		ren = append(ren, renn)
+		hend := num[rand.Intn(len(num))]
+		ren = append(ren, hand)
 	}
 	fmt.Println(ren)
 
