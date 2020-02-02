8f6eb0b9d77d30b7fb12dc1fb10adb8875b9805d
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-02 15:43:14 +0700

    Update confused.go

--- week12/card/confused.go
+++ week12/card/confused.go
@@ -5,7 +5,7 @@ import (
 	"math/rand"
 )
 
-func card() {
+func main() {
 	num := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
 	henddis := make([]string, 6)
 	for i := 0; i < 5; i++ {
