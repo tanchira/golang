db543f3156f93612246194f5592625a461c9412d
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-02 15:00:30 +0700

    Update confused.go

--- week12/card/confused.go
+++ week12/card/confused.go
@@ -3,6 +3,7 @@ package card
 import "math/rand"
 
 func main() int {
+	var pair int
 	num := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
 	ren := make([]string, 6)
 	for i := 0; i < 5; i++ {
