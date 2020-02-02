90308420f506226ee79f991b89fc7c3f411468c6
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-02 15:33:30 +0700

    Update confused.go

--- week12/card/confused.go
+++ week12/card/confused.go
@@ -10,24 +10,7 @@ func card() {
 	henddis := make([]string, 6)
 	for i := 0; i < 5; i++ {
 		hend := num[rand.Intn(len(num))]
-		henddis = append(hend)
+		henddis = append(henddis, hend)
 	}
-	fmt.Println(hend)
-}
-
-func main() int {
-	var pair int
-	num := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
-	ren := make([]string, 6)
-	for i := 0; i < 5; i++ {
-		hend := num[rand.Intn(len(num))]
-		ren = append(ren, hend)
-	}
-	fmt.Println(ren)
-	fmt.Println(pair)
-
-}
-
-func cke() {
-
+	fmt.Println(henddis)
 }
