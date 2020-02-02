7c6385c98f44c603dce4fc8f1878f4ade47698cb
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-02 16:04:38 +0700

    Update confused.go

--- week12/card/confused.go
+++ week12/card/confused.go
@@ -47,4 +47,7 @@ func Ckeckcard() int {
 	var pair int
 	num := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
 	henddis := make([]string, 6)
+	for i := 0; i < len(henddis); i++ {
+
+	}
 }
