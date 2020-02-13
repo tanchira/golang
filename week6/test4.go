7afb7db1bf9611bdea2d418573a177c58bdbc148
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-13 11:37:06 +0700

    Update test4.go

--- week6/test4.go
+++ week6/test4.go
@@ -1,11 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	for essay := 1; essay <= 20; essay = essay + 1 {
 		if essay%2 == 0 {
 			fmt.Println(essay, "even")
-		}else {
+		} else {
 			fmt.Println(essay, "odd")
 		}
 
 	}
+}
