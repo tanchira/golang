1f68e96764864feafd6d4057827f2a0d4acb92bb
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-13 11:19:37 +0700

    Delete card.go

--- week6/test1.go
+++ week6/test1.go
@@ -1,15 +1,1 @@
 package main
-
-import "fmt"
-
-func say() {
-	fmt.Println("Hello")
-}
-
-func greet(name string) {
-	fmt.Println("Hello", name)
-}
-func main() {
-	say()
-	greet("Goku")
-}
---
+++ week77/test1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func say() {
+	fmt.Println("Hello")
+}
+
+func greet(name string) {
+	fmt.Println("Hello", name)
+}
+func main() {
+	say()
+	greet("Goku")
+}
