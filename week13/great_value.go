7c4ed756ae73883ccbd81db8ab63de1843d394b7
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-15 20:15:32 +0700

    Update great_value.go

--- week13/great_value.go
+++ week13/great_value.go
@@ -6,22 +6,22 @@ func main() {
 	var num [10]int
 	var i, j, max, min []int
 
-
 	for i := 0; i < 10; i++ {
 		fmt.Println("Enter number :")
 		fmt.Scanf("%d", &num[i])
 	}
-min = num[0]
+	min = num
+	max = num
+
 	for j := 0; j <= 10; j++ {
 		if min > num[j] {
 			min = num[j]
-		}else if  
-		max < num[j] {
+		} else if max < num[j] {
 			max = num[j]
-		}else
+		} else {
+
+		}
 	}
-	fmt.Printf{"The minimum is %d maximum is %d",min,max}
- 	
+	fmt.Printf("The minimum is %d maximum is %d", min, max)
+
 }
-
-
