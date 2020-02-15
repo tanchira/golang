6bf4671bb567a69654060b185c46d8769ad7f5d8
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-15 17:30:45 +0700

    Update name_array.go

--- week13/name_array.go
+++ week13/name_array.go
@@ -1,7 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	name := []string{"Somsri", "Somjai", "Somnuk", "Somjit", "Somporn",
 		"Sombat", "Somkran", "Somtavin"}
+
+	fmt.Println("column 1				column 2")
 
 }
