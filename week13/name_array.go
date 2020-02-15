079c87ca954d813cb5a4587ea178f10c15ffdeed
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-15 17:34:10 +0700

    Update name_array.go

--- week13/name_array.go
+++ week13/name_array.go
@@ -6,8 +6,8 @@ func main() {
 	var name [8]string
 	name[0] = "Somsri"
 	name[1] = "Somjai"
-
-	name := []string{"Somsri", , "Somnuk", "Somjit", "Somporn",
+	name[2] = "Somnuk"
+	name := []string{, , , "Somjit", "Somporn",
 		"Sombat", "Somkran", "Somtavin"}
 
 	fmt.Println("column 1				column 2")
