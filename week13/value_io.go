83ead65c08393e4cf12bdc6a5468c654d62f62fc
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-15 20:33:16 +0700

    Update value_io.go

--- week13/value_io.go
+++ week13/value_io.go
@@ -9,10 +9,10 @@ func main() {
 	b := 2
 	c := b * a
 	d := c + a
-	e := d / a
+	e = d / a
 
 	fmt.Printf("value of c %d\n", c)
 	fmt.Printf("value of d %d\n", d)
-	fmt.Printf("value of e %f\n", e)
+	fmt.Printf("value of e %f\n", &e)
 
 }
