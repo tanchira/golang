be95f41c4f9bf1bc2e05f2db6eec8fbac1ef60fd
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-15 20:31:59 +0700

    Update value_io.go

--- week13/value_io.go
+++ week13/value_io.go
@@ -3,16 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var a, b, c, d int
-	var e float64
-	a = 10
-	b = 2
-	c = b * a
-	d = c + a
-	e = d / a
+
+	a := 10
+	b := 2
+	c := b * a
+	d := c + a
+	e := d / a
 
 	fmt.Printf("value of c %d\n", c)
 	fmt.Printf("value of d %d\n", d)
 	fmt.Printf("value of e %f\n", e)
-	return 0
+
 }
