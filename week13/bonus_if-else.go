1df86c1873ff10bd26ead82596c545effbf2a3e4
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-16 18:42:30 +0700

    Update bonus_if-else.go

--- week13/bonus_if-else.go
+++ week13/bonus_if-else.go
@@ -3,8 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	Hugesales := 50000 / 100 * 30
-	sales := 49999 / 100 * 10
 	fmt.Println("			Report  Bonus			")
 	fmt.Println("*************************************************")
 	var no int
