ab009f4705c22a05b9932e47d5903f17fa5b956a
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-16 23:18:09 +0700

    Update cost.go

--- week13/cost.go
+++ week13/cost.go
@@ -1,10 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	var elecbill int
 	var waterbill int
 	elecbill = elecbill * 5
 	waterbill = waterbill * 2
+	fmt.Println("waterbill", waterbill)
 	elecbill = elecbill + ((elecbill * 7) / 100)
 	waterbill = waterbill + ((waterbill * 7) / 100)
 	all = elecbill + waterbill
