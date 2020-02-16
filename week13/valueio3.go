895b9618b0704b28e0bae1477578211bde2972ff
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-16 13:42:23 +0700

    Update valueio3.go

--- week13/valueio3.go
+++ week13/valueio3.go
@@ -13,6 +13,6 @@ func main() {
 	fmt.Printf("%d \n", value1+value2)
 	fmt.Printf("%d \n", value1-value2)
 	fmt.Printf("%d \n", value1*value2)
-	fmt.Printf("%d \n", value1+value2)
+	fmt.Printf("%d \n", value1/value2)
 
 }
