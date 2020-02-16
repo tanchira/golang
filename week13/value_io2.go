2058819c85c1f6e7bd7b692316654dde970ab848
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-16 13:29:29 +0700

    Update value_io2.go

--- week13/value_io2.go
+++ week13/value_io2.go
@@ -7,8 +7,8 @@ func main() {
 	value2 := 6
 	value3 := 0
 
-	c := value1 + value2
+	value3 = value1 + value2
 	fmt.Printf("%d %d %d\n", value1, value2, value3)
-	c := value1 - value2
+	value3 = value1 - value2
 
 }
