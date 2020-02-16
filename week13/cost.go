35cad0095aed5ba5cdcb0164da237d6a4eeb9add
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-16 23:19:07 +0700

    Update cost.go

--- week13/cost.go
+++ week13/cost.go
@@ -8,7 +8,7 @@ func main() {
 	elecbill = elecbill * 5
 	waterbill = waterbill * 2
 	fmt.Println("Water bill", waterbill)
-	fmt.Println("Electricity bill")
+	fmt.Println("Electricity bill", elecbill)
 	elecbill = elecbill + ((elecbill * 7) / 100)
 	waterbill = waterbill + ((waterbill * 7) / 100)
 	all = elecbill + waterbill
