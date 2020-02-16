97e9fa8136a205704713522a099b98697f021018
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-16 18:24:20 +0700

    Update salary_if-else.go

--- week13/salary_if-else.go
+++ week13/salary_if-else.go
@@ -7,11 +7,11 @@ func main() {
 	fmt.Println("salary : ")
 	fmt.Scan(&salary)
 
-	deduct2 := 5001 / (100 * 5)
 	if salary >= 10000 {
 		deduct := salary / 100 * 10
 		fmt.Println("หักภาษีทั้งหมด", deduct)
 	} else if salary >= 5001 {
+		deduct := salary / 100 * 5
 		fmt.Println("ให้หักภาษีทั้งหมด", deduct2)
 	}
 
