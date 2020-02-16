1e41800c23d31eb8a0dace41ec675029022b322d
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-16 13:50:02 +0700

    Update value_io3.go

--- week13/value_io3.go
+++ week13/value_io3.go
@@ -1,23 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	var value1, value2 int
-	var value3, value4 float64
-
-	value1 = 10
-	value3 = 5.5
-	value2 = 3
-	value4 = 3.5
-	fmt.Printf("%d \n", value1+value2)
-	fmt.Printf("%d \n", value1-value2)
-	fmt.Printf("%d \n", value1*value2)
-	fmt.Printf("%d \n", value1/value2)
-	fmt.Printf("%d \n", value1%value2)
-	fmt.Printf("%f \n", value3+value4)
-	fmt.Printf("%f \n", value3-value4)
-	fmt.Printf("%f \n", value3*value4)
-	fmt.Printf("%f \n", value3/value4)
 
 }
