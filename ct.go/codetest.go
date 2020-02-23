81e64ca01625720a5b88fee90dfe8e810aa22e24
tanchira <57671539+tanchira@users.noreply.github.com>
2020-02-23 22:18:28 +0700

    Update codetest.go

--- ct.go/codetest.go
+++ ct.go/codetest.go
@@ -46,5 +46,6 @@ func findFileFromExtention(area map[string]string, dir string, files *[]file) {
 }
 func createfile(path, size, name []string) {
 	file, err := os.Create("Output.txt")
+	defer file,Close()
 
 }
